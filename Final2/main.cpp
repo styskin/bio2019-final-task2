b56a9a86eed995ce80c93e8cb098ea5512a6d99a
Andrey Styskin <styskin@macbook-air-6.local>
2019-02-24 00:31:46 +0300

    K-means for clusterization was added

--- Final2/main.cpp
+++ Final2/main.cpp
@@ -16,8 +16,9 @@
 #include <math.h>
 using namespace std;
 
-typedef bitset<5> TBS;
-typedef pair<int, TBS> pib;
+typedef bitset<100> TBS;
+typedef vector<int> BS;
+typedef pair<int, BS> pib;
 
 
 bool wayToSort(const pib& i, const pib& j) {
@@ -27,106 +28,108 @@ bool wayToSort(const pib& i, const pib& j) {
 
 
 int main(int argc, const char * argv[]) {
-    string path("/Users/styskin/bio2019/Final2/");
-    ifstream in;
-    in.open(path + "input_1.txt");
-    ofstream out;
-    out.open(path + "output_1.out");
-    int K;
-    in >> K;
-    vector<string> M;
-    string s;
-    getline(in, s);
-    while (getline(in, s)) {
-        M.push_back(s);
-    }
-    unordered_map<TBS, int> count;
-    
-    size_t N = M.size();
-    for (int i = 0; i < M[0].length(); ++i) {
-        TBS state;
-        for (int j = 0; j < N; ++j) {
-            state[j] = M[j][i] == '1';
+    for (int T = 1; T <= 4; ++T) {
+        string path("/Users/styskin/bio2019/Final2/");
+        ifstream in;
+        in.open(path + "input_" + to_string(T) + ".txt");
+        ofstream out;
+        out.open(path + "output_" + to_string(T) +  ".out");
+        int K;
+        in >> K;
+        vector<string> M;
+        string s;
+        getline(in, s);
+        while (getline(in, s)) {
+            M.push_back(s);
         }
-        ++count[state];
-    }
-    // Get top K states and code them
-    vector<pair<int, TBS> > can_states;
-    for (auto& a : count) {
-        can_states.push_back(pair<int, TBS>(a.second, a.first));
-    }
-
-    int KK = K + 1000;
-
-    
-    sort(can_states.begin(), can_states.end(), wayToSort);
-//    for (int i = 0; i < KK; ++i) {
-//        cout << can_states[i].first << " " << can_states[i].second.to_string() << endl;
-//    }
-    
-    
-    // TRY GET MINS
-    vector<int> MINS(KK);
-    
-    for (int i = 0; i < M[0].length(); ++i) {
-        TBS state;
-        for (int j = 0; j < N; ++j) {
-            state[j] = M[j][i] == '1';
-        }
-        // Choose min state
-        int mn = 0;
-        int diff = 100000000;
-        for (int k = 0; k < KK; ++k) {
-            int d = 0;
+        unordered_map<TBS, int> count;
+        
+        size_t N = M.size();
+        for (int i = 0; i < M[0].length(); ++i) {
+            TBS state;
             for (int j = 0; j < N; ++j) {
-                if (state[j] != can_states[k].second[j]) {
-                    ++d;
+                state[j] = M[j][i] == '1';
+            }
+            ++count[state];
+        }
+        // Get top K states and code them
+        vector<pair<int, BS> > can_states;
+        for (auto& a : count) {
+            BS bs;
+            for (int i = 0; i < N; ++i) {
+                bs.push_back(a.first[i]);
+            }
+            can_states.push_back(pair<int, BS>(a.second, bs));
+        }
+        
+        sort(can_states.begin(), can_states.end(), wayToSort);
+        for (int i = 0; i < can_states.size(); ++i) {
+            can_states[i].first = 1;
+        }
+        // K-means analogue
+        for (int i = 0; i < M[0].length(); ++i) {
+            TBS state;
+            for (int j = 0; j < N; ++j) {
+                state[j] = M[j][i] == '1';
+            }
+            // Choose min state
+            int mn = 0;
+            int diff = 100000000;
+            for (int k = 0; k < K; ++k) {
+                int d = 0;
+                for (int j = 0; j < N; ++j) {
+                    if (state[j] != (can_states[k].second[j] > can_states[k].first / 2) ) {
+                        ++d;
+                    }
+                }
+                if (d < diff) {
+                    mn = k;
+                    diff = d;
                 }
             }
-            if (d < 10) {
-                MINS[k] += N - d;
-            }
-            if (d < diff) {
-                mn = k;
-                diff = d;
+            
+            // UPDATE K-means
+            ++can_states[mn].first;
+            for (int j = 0; j < N; ++j) {
+                can_states[mn].second[j] += state[j];
             }
         }
-    }
-    vector<pair<int, TBS> > states;
-    for (int k = 0; k < KK; ++k) {
-        cout << MINS[k] << " vs " << can_states[k].first << endl;
-        states.push_back(pair<int, TBS>(MINS[k], can_states[k].second));
-    }
-    
-    sort(states.begin(), states.end(), wayToSort);
-
-    // OUTPUT
-    for (int i = 0; i < M[0].length(); ++i) {
-        TBS state;
-        for (int j = 0; j < N; ++j) {
-            state[j] = M[j][i] == '1';
-        }
-        // Choose min state
-        int mn = 0;
-        int diff = 100000000;
+        
         for (int k = 0; k < K; ++k) {
-            int d = 0;
+            cout << "Clastered: " << can_states[k].first << endl;
+        }
+        
+        // OUTPUT
+        for (int i = 0; i < M[0].length(); ++i) {
+            TBS state;
             for (int j = 0; j < N; ++j) {
-                if (state[j] != states[k].second[j]) {
-                    ++d;
+                state[j] = M[j][i] == '1';
+            }
+            // Choose min state
+            int mn = 0;
+            int diff = 100000000;
+            for (int k = 0; k < K; ++k) {
+                int d = 0;
+                for (int j = 0; j < N; ++j) {
+                    if (state[j] != (can_states[k].second[j] > can_states[k].first / 2)) {
+                        ++d;
+                    }
+                }
+                if (d < diff) {
+                    mn = k;
+                    diff = d;
                 }
             }
-            if (d < diff) {
-                mn = k;
-                diff = d;
+            // mn - state
+            if (i > 0) {
+                out << " ";
             }
+            out << mn;
         }
-        // mn - state
-        if (i > 0) {
-            out << " ";
-        }
-        out << mn;
+        out << endl;
+        
+        in.close();
+        out.close();
     }
-    out << endl;
     return 0;
 }

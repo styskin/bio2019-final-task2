7e728dd6d361d46d85a19c5dcaa10f6a04c6f20f
Andrey Styskin <styskin@macbook-air-6.local>
2019-02-24 16:59:42 +0300

    Random staring points were added

--- Final2/main.cpp
+++ Final2/main.cpp
@@ -14,9 +14,12 @@
 #include <unordered_map>
 #include <stdlib.h>
 #include <math.h>
+#include <algorithm>
+#include <random>
+
 using namespace std;
 
-typedef bitset<100> TBS;
+typedef bitset<5> TBS;
 typedef vector<int> BS;
 typedef pair<int, BS> pib;
 
@@ -25,10 +28,18 @@ bool wayToSort(const pib& i, const pib& j) {
     return i.first > j.first;
 }
 
+int myrandom (int i) {
+    return std::rand() % i;
+}
+
+int min(int a, int b) {
+    return a < b ? a : b;
+}
 
 
 int main(int argc, const char * argv[]) {
     for (int T = 1; T <= 4; ++T) {
+//        cout << "=========== Test " << T << endl;
         string path("/Users/styskin/bio2019/Final2/");
         ifstream in;
         in.open(path + "input_" + to_string(T) + ".txt");
@@ -42,46 +53,71 @@ int main(int argc, const char * argv[]) {
         while (getline(in, s)) {
             M.push_back(s);
         }
-        unordered_map<TBS, int> count;
-        
         size_t N = M.size();
-        for (int i = 0; i < M[0].length(); ++i) {
-            TBS state;
-            for (int j = 0; j < N; ++j) {
-                state[j] = M[j][i] == '1';
-            }
-            ++count[state];
-        }
-        // Get top K states and code them
-        vector<pair<int, BS> > can_states;
-        for (auto& a : count) {
-            BS bs;
-            for (int i = 0; i < N; ++i) {
-                bs.push_back(a.first[i]);
-            }
-            can_states.push_back(pair<int, BS>(a.second, bs));
-        }
+        int KK = K;
         
-        sort(can_states.begin(), can_states.end(), wayToSort);
-        for (int i = 0; i < can_states.size(); ++i) {
-            can_states[i].first = 1;
+        vector<int> shuffled;
+        for (int i = 0; i < M[0].length(); ++i) {
+            shuffled.push_back(i);
         }
+        std::srand ( unsigned ( std::time(0) ) );
+        random_shuffle(shuffled.begin(), shuffled.end(), myrandom);
+        
+        
+        // GET RANDOM STARTING POINTS
+
+        vector<pair<int, BS> > can_states;
+        for (int i = 0; i < KK; ++i) {
+            BS bs(N);
+            for (int j = 0; j < N; ++j) {
+                bs[j] = M[j][shuffled[i]] == '1';
+            }
+            can_states.push_back(pair<int, BS>(0, bs));
+        }
+
+        
+//        unordered_map<TBS, int> count;
+//        for (int i = 0; i < M[0].length(); ++i) {
+//            TBS state;
+//            for (int j = 0; j < N; ++j) {
+//                state[j] = M[j][i] == '1';
+//            }
+//            ++count[state];
+//        }
+//        vector<pair<int, BS> > can_states;
+//        for (auto& a : count) {
+//            BS bs;
+//            for (int i = 0; i < N; ++i) {
+//                bs.push_back(a.first[i]);
+//            }
+//            can_states.push_back(pair<int, BS>(a.second, bs));
+//        }
+//        sort(can_states.begin(), can_states.end(), wayToSort);
+//        for (int i = 0; i < can_states.size(); ++i) {
+//            can_states[i].first = 0;
+//        }
+
+
+        
         // K-means analogue
         for (int i = 0; i < M[0].length(); ++i) {
             TBS state;
             for (int j = 0; j < N; ++j) {
-                state[j] = M[j][i] == '1';
+                state[j] = M[j][shuffled[i]] == '1';
             }
             // Choose min state
             int mn = 0;
             int diff = 100000000;
-            for (int k = 0; k < K; ++k) {
+            for (int k = 0; k < KK; ++k) {
                 int d = 0;
                 for (int j = 0; j < N; ++j) {
                     if (state[j] != (can_states[k].second[j] > can_states[k].first / 2) ) {
                         ++d;
                     }
                 }
+                // Distance
+                d = min(d, N - d);
+                
                 if (d < diff) {
                     mn = k;
                     diff = d;
@@ -95,10 +131,90 @@ int main(int argc, const char * argv[]) {
             }
         }
         
-        for (int k = 0; k < K; ++k) {
-            cout << "Clastered: " << can_states[k].first << endl;
+        // Output clusters
+//        int total = 0;
+//        for (int k = 0; k < KK; ++k) {
+//            cout << "Clastered: " << can_states[k].first << " ";
+//            total += can_states[k].first;
+//            for (int j = 0; j < N; ++j) {
+//                cout << (can_states[k].second[j] > can_states[k].first / 2 ? 1 : 0);
+//            }
+//            cout << endl;
+//        }
+//        cout << total << " from " << M[0].length() << endl;
+        
+        can_states.clear();
+        can_states.push_back(pair<int, BS>(1, BS(5)));
+        can_states.push_back(pair<int, BS>(1, BS(5)));
+        can_states.push_back(pair<int, BS>(1, BS(5)));
+        can_states.push_back(pair<int, BS>(1, BS(5)));
+        can_states[0].second = vector<int>({0, 0, 0, 0, 0});
+        can_states[1].second = vector<int>({0, 0, 1, 0, 0});
+        can_states[2].second = vector<int>({0, 1, 0, 0, 0});
+        can_states[3].second = vector<int>({0, 1, 1, 0, 0});
+
+
+        
+
+        if (0) {
+            int totaldiff = 100000000;
+            vector<TBS> res;
+            
+            for (int i1 = 0; i1 < (1 << 5); ++i1) {
+                for (int i2 = i1 + 1; i2 < (1 << 5); ++i2) {
+                    for (int i3 = i2 + 1; i3 < (1 << 5); ++i3) {
+                        for (int i4 = i3 + 1; i4 < (1 << 5); ++i4) {
+                            vector<TBS> m;
+                            m.push_back(TBS(i1));
+                            m.push_back(TBS(i2));
+                            m.push_back(TBS(i3));
+                            m.push_back(TBS(i4));
+
+                            int tdiff = 0;
+                            for (int i = 0; i < M[0].length(); ++i) {
+                                TBS state;
+                                for (int j = 0; j < N; ++j) {
+                                    state[j] = M[j][i] == '1';
+                                }
+                                // Choose min state
+                                int mn = 0;
+                                int diff = 100000000;
+                                for (int k = 0; k < m.size(); ++k) {
+                                    int d = 0;
+                                    for (int j = 0; j < N; ++j) {
+                                        if (state[j] != m[k][j]) {
+                                            ++d;
+                                        }
+                                    }
+                                    
+                                    d = min(d, N - d);
+                                    if (d < diff) {
+                                        mn = k;
+                                        diff = d;
+                                    }
+                                }
+                                tdiff += diff;
+                            }
+                            if (tdiff < totaldiff) {
+                                totaldiff = tdiff;
+                                cout << "New min " << tdiff << endl;
+                                res = m;
+                            }
+                        }
+                    }
+                }
+            }
+            cout << totaldiff << endl;
+            for (int j = 0; j < res.size(); ++j) {
+                cout << res[j].to_string() << endl;
+            }
+
+
         }
         
+        
+        
+        int totaldiff = 0;
         // OUTPUT
         for (int i = 0; i < M[0].length(); ++i) {
             TBS state;
@@ -108,18 +224,22 @@ int main(int argc, const char * argv[]) {
             // Choose min state
             int mn = 0;
             int diff = 100000000;
-            for (int k = 0; k < K; ++k) {
+            for (int k = 0; k < KK; ++k) {
                 int d = 0;
                 for (int j = 0; j < N; ++j) {
                     if (state[j] != (can_states[k].second[j] > can_states[k].first / 2)) {
                         ++d;
                     }
                 }
+                //Distance
+                d = min(d, N - d);
+
                 if (d < diff) {
                     mn = k;
                     diff = d;
                 }
             }
+            totaldiff += diff;
             // mn - state
             if (i > 0) {
                 out << " ";
@@ -127,6 +247,7 @@ int main(int argc, const char * argv[]) {
             out << mn;
         }
         out << endl;
+        cout << "Test " << T << ": " << totaldiff << endl;
         
         in.close();
         out.close();
